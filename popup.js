20be847a59375e8e1c1d0ed883cd8d1a3ac20207
Amar Jupalli <43963643+amarjupalli@users.noreply.github.com>
2025-01-15 09:56:32 -0800

    style: enhance popup UI and improve release info display

--- popup.js
+++ popup.js
@@ -4,17 +4,27 @@
       active: true,
       currentWindow: true,
     });
-    if (tab && tab.id) {
+    if (tab?.id) {
       const results = await chrome.scripting.executeScript({
         target: { tabId: tab.id },
         func: () => {
           const allScripts = Array.from(document.scripts);
           for (const script of allScripts) {
             const content = script.textContent;
+            /**
+             * This regex will match the following patterns newrelic.addRelease('name', 'version') and newrelic.addRelease("name", "version")
+             * the match array will look like this:
+             * 0: "newrelic.addRelease('name', 'version')"
+             * 1: "'" or '"'
+             * 2: "name"
+             * 3: "'" or '"'
+             * 4: "version
+             */
             const regex =
               /newrelic\.addRelease\((['"])(.*?)\1,\s*(['"])(.*?)\3\)/i;
             const match = content.match(regex);
             if (match) {
+              // return the indexes 2 and 4 of the match array to get rlease name and release version
               return { name: match[2], version: match[4] };
             }
           }
@@ -24,8 +34,8 @@
 
       const releaseInfo = results[0]?.result;
       document.getElementById("releaseInfo").textContent = releaseInfo
-        ? `Release Name: ${releaseInfo.name}, Release Version: ${releaseInfo.version}`
-        : "No newRelic.addRelease call found on this page.";
+        ? `${releaseInfo.name}: v${releaseInfo.version}`
+        : "No release found on this page.";
     } else {
       document.getElementById("releaseInfo").textContent =
         "Unable to access the current tab.";
